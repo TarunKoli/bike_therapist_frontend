f7eb3014c280735f9e0e8d9b5205a20656263819
TarunKoli <amankumar8348@gmail.com>
2021-04-24 00:27:17 +0530

    Adding Service Worker

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const withPWA = require("next-pwa");
 
 module.exports = withPWA({
   pwa: {
-    disable: process.env.NODE_ENV === "development",
+    // disable: process.env.NODE_ENV === "development",
+    disable: false,
     register: true,
     dest: "public",
     sw: "service-worker.js",
